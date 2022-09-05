d15502f6794734d3c5da6dba684bb23fcf104000
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-05 12:22:17 +0530

    Update App.java

--- src/main/java/eygds/maven/demo/App.java
+++ src/main/java/eygds/maven/demo/App.java
@@ -1,5 +1,7 @@
 package eygds.maven.demo;
 
+import java.util.Scanner;
+
 import eygds.maven.demo.maths.Calc;
 
 /**
@@ -10,13 +12,21 @@ public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		System.out.println("Start...");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please enter num1");
+		int num1 = sc.nextInt();
+		System.out.println("Please enter num2");
+		int num2 = sc.nextInt();
 
 		Calc calc = new Calc();
 
-		System.out.println(calc.addNums(10, 20));
-		System.out.println(calc.subNums(10, 5));
+		System.out.println(calc.addNums(num1, num2));
+		System.out.println(calc.subNums(num2, num2));
+
+		sc.close();
+		System.out.println("End");
 
 	}
 }
-
