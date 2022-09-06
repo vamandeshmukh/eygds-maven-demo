3c9bba63827c0dc1122a9aa17648fd16f42a3d00
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-06 11:04:40 +0530

    json

--- src/test/java/eygds/maven/demo/maths/CalcTests.java
+++ src/test/java/eygds/maven/demo/maths/CalcTests.java
@@ -1,5 +1,24 @@
 package eygds.maven.demo.maths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CalcTests {
 
+	@Test
+	public void testAddNums() {
+		Calc calc = new Calc();
+		int expected = 10;
+		int actual = calc.addNums(5, 5);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testSubNums() {
+		Calc calc = new Calc();
+		int expected = 5;
+		int actual = calc.subNums(10, 5);
+		assertEquals(expected, actual);
+	}
 }
