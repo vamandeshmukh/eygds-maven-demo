812dd923839984fcaea225a8fdcf9728bb229a83
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-06 11:48:51 +0530

    JUnit

--- src/test/java/eygds/maven/demo/maths/CalcTests.java
+++ src/test/java/eygds/maven/demo/maths/CalcTests.java
@@ -7,16 +7,18 @@ import org.junit.jupiter.api.Test;
 
 // test suites    
 public class CalcTests {
+	
+	Calc calc = new Calc();
 
 	@Test // positive test case
 	public void testAddNums() {
-		Calc calc = new Calc();
+		calc = new Calc();
 		assertEquals(10, calc.addNums(5, 5));
 	}
 
 	@Test // negative test case
 	public void testAddNums2() {
-		Calc calc = new Calc();
+		calc = new Calc();
 		int unexpected = 11;
 		int actual = calc.addNums(5, 5);
 		assertNotEquals(unexpected, actual);
@@ -24,7 +26,7 @@ public class CalcTests {
 
 	@Test
 	public void testSubNums() {
-		Calc calc = new Calc();
+		calc = new Calc();
 		int expected = 5;
 		int actual = calc.subNums(10, 5);
 		assertEquals(expected, actual);
@@ -32,7 +34,7 @@ public class CalcTests {
 
 	@Test
 	public void testSubNums2() {
-		Calc calc = new Calc();
+		calc = new Calc();
 		int unexpected = 66;
 		int actual = calc.subNums(10, 5);
 		assertNotEquals(unexpected, actual);
