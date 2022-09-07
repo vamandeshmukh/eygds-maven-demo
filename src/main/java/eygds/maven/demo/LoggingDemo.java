758e88f1deb1b4c9c4bf4d4eea0e9b481d7490ef
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-07 15:49:51 +0530

    LOG4J

--- src/main/java/eygds/maven/demo/LoggingDemo.java
+++ src/main/java/eygds/maven/demo/LoggingDemo.java
@@ -3,6 +3,8 @@ package eygds.maven.demo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+// https://logging.apache.org/log4j/2.x/manual/configuration.html
+
 public class LoggingDemo {
 
 	static Logger logger = LogManager.getLogger(LoggingDemo.class);
